3f4e08f44c0ad97770d5ccbd981478ee82e97eb8
Forrest Hopkins <fhopkins404@gmail.com>
2020-11-22 06:59:51 -0700

    Generate manifests dynamically

--- src/app.js
+++ src/app.js
@@ -1,43 +1,80 @@
 // Dependencies
 const http = require('http');
 const express = require('express');
+const config = require('../resources/config.json');
 
 const app = express();
 
-const url = ''; // set this
 const port = 3001;
 
+app.set('trust proxy', 'loopback');
+
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url} => ${res.statusCode}`);
+  console.log(`${req.protocol} ${req.method} ${req.url} => ${res.statusCode}`);
   next();
 });
 
-// copy-paste this and the source functions for every movie
-app.get('/frozen.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.json({
-    title: 'Frozen',
-    duration: 6432, // set this to actual duration (in seconds)
-    live: false,
-    sources: [
-      {
-        url: `${url}/frozen1080`,
-        contentType: 'video/mp4',
-        quality: 1080,
-      },
-      {
-        url: `${url}/frozen720`,
-        contentType: 'video/mp4',
-        quality: 720,
-      },
-      {
-        url: `${url}/frozen480`,
-        contentType: 'video/mp4',
-        quality: 480,
-      },
-    ],
+console.log(JSON.stringify(config, null, ' '));
+
+const mediaDefaults = {
+  title: 'Untitled',
+  live: false,
+  thumbnail: null,
+};
+
+const sourceDefaults = {
+  contentType: 'video/mp4',
+  quality: 1080,
+};
+
+const trackDefaults = {
+  contentType: 'text/vtt',
+  name: 'Untitled',
+  default: false,
+};
+
+function addJsonRoute(id, getManifest) {
+  app.get(`/${id}.json`, (req, res) => {
+    res.json(getManifest(req.hostname));
   });
-});
+}
+
+function createMediaUrl(hostname, filename) {
+  return `https://${hostname}/media/${filename}`;
+}
+
+function toSource(hostname, file, data) {
+  return {
+    ...sourceDefaults,
+    url: createMediaUrl(hostname, file),
+    ...data,
+  };
+}
+
+function toTrack(hostname, file, data) {
+  return {
+    ...trackDefaults,
+    url: createMediaUrl(hostname, file),
+    ...data,
+  };
+}
+
+function toManifest(hostname, data) {
+  return {
+    ...mediaDefaults,
+    ...data,
+    sources: Object.entries(data.sources).map(([k, v]) =>
+      toSource(hostname, k, v),
+    ),
+    textTracks:
+      data.textTracks &&
+      Object.entries(data.textTracks).map(([k, v]) => toTrack(hostname, k, v)),
+  };
+}
+
+Object.entries(config.media).forEach(([k, v]) =>
+  addJsonRoute(k, (hostname) => toManifest(hostname, v)),
+);
 
 // Starting server
 const httpServer = http.createServer(app);
