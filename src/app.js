d5e799e307cda8ee3b87a7d27459f0f2ccc54c3e
Forrest Hopkins <fhopkins404@gmail.com>
2020-11-23 21:16:41 -0700

    Add index page listing media

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 // Dependencies
-const http = require('http');
 const express = require('express');
 const config = require('../resources/config.json');
 
 const app = express();
 
-const port = 3001;
+const listenPort = 3001;
 
 app.set('trust proxy', 'loopback');
 
@@ -14,7 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 
-console.log(JSON.stringify(config, null, ' '));
+console.log(JSON.stringify(config, null, '  '));
 
 const mediaDefaults = {
   title: 'Untitled',
@@ -34,50 +33,93 @@ const trackDefaults = {
 
 function addJsonRoute(id, getManifest) {
   app.get(`/${id}.json`, (req, res) => {
-    res.json(getManifest(req.hostname));
+    res.json(getManifest(req));
   });
 }
 
-function createMediaUrl(hostname, filename) {
-  return `https://${hostname}/media/${filename}`;
+function getBaseUrl(req) {
+  const reqPort = req.get('X-Forwarded-Port') ?? listenPort;
+
+  let expectedPort;
+  if (req.protocol === 'http') expectedPort = 80;
+  else if (req.protocol === 'https') expectedPort = 443;
+
+  const port = reqPort !== expectedPort ? `:${reqPort}` : '';
+  return `${req.protocol}://${req.hostname}${port}`;
 }
 
-function toSource(hostname, file, data) {
+function createMediaUrl(req, filename) {
+  return `${getBaseUrl(req)}/media/${filename}`;
+}
+
+function createManifestUrl(req, id) {
+  return `${getBaseUrl(req)}/${id}.json`;
+}
+
+function toSource(req, file, data) {
   return {
     ...sourceDefaults,
-    url: createMediaUrl(hostname, file),
+    url: createMediaUrl(req, file),
     ...data,
   };
 }
 
-function toTrack(hostname, file, data) {
+function toTrack(req, file, data) {
   return {
     ...trackDefaults,
-    url: createMediaUrl(hostname, file),
+    url: createMediaUrl(req, file),
     ...data,
   };
 }
 
-function toManifest(hostname, data) {
+function toManifest(req, data) {
   return {
     ...mediaDefaults,
     ...data,
-    sources: Object.entries(data.sources).map(([k, v]) =>
-      toSource(hostname, k, v),
-    ),
+    sources: Object.entries(data.sources).map(([k, v]) => toSource(req, k, v)),
     textTracks:
       data.textTracks &&
-      Object.entries(data.textTracks).map(([k, v]) => toTrack(hostname, k, v)),
+      Object.entries(data.textTracks).map(([k, v]) => toTrack(req, k, v)),
   };
 }
 
-Object.entries(config.media).forEach(([k, v]) =>
-  addJsonRoute(k, (hostname) => toManifest(hostname, v)),
-);
+const manifests = [];
+
+Object.entries(config.media).forEach(([k, v]) => {
+  addJsonRoute(k, (req) => toManifest(req, v));
+  manifests.push({ id: k, title: v.title });
+});
+
+app.get('/', (req, res) => {
+  const links = manifests.map(({ id, title }) => ({
+    title,
+    url: createManifestUrl(req, id),
+  }));
+  res.status(200).send(`
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Link</th>
+      </tr>
+    </thead>
+  <tbody>
+    ${links
+      .map(
+        ({ title, url }) => `
+      <tr>
+        <td>${title}</td>
+        <td><a href="${url}">${url}</a></td>
+      </tr>
+    `,
+      )
+      .join('')}
+  </tbody>
+  </table>
+  `);
+});
 
 // Starting server
-const httpServer = http.createServer(app);
-
-httpServer.listen(port, () => {
-  console.log(`HTTP Server running on port ${port}`);
+app.listen(listenPort, () => {
+  console.log(`HTTP Server running on port ${listenPort}`);
 });
